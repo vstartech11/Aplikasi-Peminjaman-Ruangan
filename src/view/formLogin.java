1a44e30d15b7c507a98e2d743bedb9ec18e30fa3
Dimas Pramudya <11211032@student.itk.ac.id>
2022-06-09 13:53:25 +0800

    09-06-2022

--- src/view/formLogin.java
+++ src/view/formLogin.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package aplikasi.peminjaman.ruangan.view;
+package view;
 import java.sql.*;
 import javax.swing.JOptionPane;
 
