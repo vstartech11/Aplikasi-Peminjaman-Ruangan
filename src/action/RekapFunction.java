6fc4b51648909a39b5626ffc7bcda0f62e46998c
dimpram101 <64238889+dimpram101@users.noreply.github.com>
2022-06-13 14:24:03 +0800

    rekap new

--- src/action/RekapFunction.java
+++ src/action/RekapFunction.java
@@ -1,7 +1,38 @@
 package action;
 
 import view.RekapLayout;
+import java.sql.*;
+import javax.swing.table.*;
 
 public class RekapFunction extends RekapLayout {
-  
+  Connection conn = null;
+  DefaultTableModel model;
+  ResultSet rs = null;
+  Statement st = null;
+  int rows = 0;
+
+  public RekapFunction() {
+    conn = Koneksi.koneksi();
+  }
+
+  private void getTabel() {
+    String sql = "Select tblRuangan.idRuangan,tblRuangan.namaRuangan,tblFasilitas.namaFasilitas,tblRuangan.kapRuangan,tblRuangan.statSesi1,tblRuangan.statSesi2,tblRuangan.statSesi3,tblRuangan.statSesi4 from tblRuangan join tblFasilitas on tblRuangan.idFasilitas=tblFasilitas.idFasilitas";
+    String[] column = { "Id Ruangan", "Kode Ruangan", "Fasilitas", "Kursi Max", "Sesi 1", "Sesi 2", "Sesi 3",
+            "Sesi 4" };
+    model = new DefaultTableModel(column, 0);
+    tableContent.setModel(model);
+    try {
+        st = conn.createStatement();
+        rs = st.executeQuery(sql);
+        while (rs.next()) {
+            model.addRow(new String[] { rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+                    rs.getString(5),
+                    rs.getString(6),
+                    rs.getString(7), rs.getString(8) });
+        }
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+  }
+
 }
--- src/view/RekapLayout.java
+++ src/view/RekapLayout.java
@@ -8,7 +8,7 @@ public class RekapLayout extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tableContent = new javax.swing.JTable();
         kolomCariInput = new javax.swing.JTextField();
         cariButton = new javax.swing.JButton();
         kembaliButton = new javax.swing.JButton();
@@ -18,7 +18,7 @@ public class RekapLayout extends javax.swing.JFrame {
         setMaximumSize(new java.awt.Dimension(1080, 720));
         setMinimumSize(new java.awt.Dimension(1080, 720));
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tableContent.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -29,7 +29,7 @@ public class RekapLayout extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(tableContent);
 
         cariButton.setText("Cari");
         cariButton.addActionListener(new java.awt.event.ActionListener() {
@@ -126,7 +126,7 @@ public class RekapLayout extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cariButton;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    public javax.swing.JTable tableContent;
     private javax.swing.JButton kembaliButton;
     private javax.swing.JTextField kolomCariInput;
     // End of variables declaration//GEN-END:variables
