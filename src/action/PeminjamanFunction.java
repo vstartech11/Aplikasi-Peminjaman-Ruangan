52d8b0fc2b98ef72533633b1d5d9d58098bb66e0
vstartech11 <11211024@student.itk.ac.id>
2022-06-13 13:37:24 +0800

    UPDATE BANYAK

--- src/action/PeminjamanFunction.java
+++ src/action/PeminjamanFunction.java
@@ -1,9 +1,19 @@
 package action;
 
 import view.PeminjamanRuanganLayout;
+import java.sql.*;
+import javax.swing.table.*;
 
-public class PeminjamanFunction extends PeminjamanRuanganLayout{
+public class PeminjamanFunction extends PeminjamanRuanganLayout {
+    Connection conn = null;
+    DefaultTableModel model;
+    ResultSet rs = null;
+    Statement st = null;
+    int rows = 0;
+
     public PeminjamanFunction() {
+        conn = Koneksi.koneksi();
+        getTabel();
         this.backButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 backButtonActionPerformed(evt);
@@ -11,18 +21,39 @@ public class PeminjamanFunction extends PeminjamanRuanganLayout{
         });
     }
 
-    private void backButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
-        setVisible(false);
-    
+    private void getTabel() {
+        String sql = "Select tblRuangan.idRuangan,tblRuangan.namaRuangan,tblFasilitas.namaFasilitas,tblRuangan.kapRuangan,tblRuangan.statSesi1,tblRuangan.statSesi2,tblRuangan.statSesi3,tblRuangan.statSesi4 from tblRuangan join tblFasilitas on tblRuangan.idFasilitas=tblFasilitas.idFasilitas";
+        String[] column = { "Id Ruangan", "Kode Ruangan", "Fasilitas", "Kursi Max", "Sesi 1", "Sesi 2", "Sesi 3",
+                "Sesi 4" };
+        model = new DefaultTableModel(column, 0);
+        tableContent.setModel(model);
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+            while (rs.next()) {
+                model.addRow(new String[] { rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7), rs.getString(8) });
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
-    public void pinjamButtonActionPerformed(java.awt.event.ActionEvent evt){
+
+    private void backButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        dispose();
+
+    }
+
+    public void pinjamButtonActionPerformed(java.awt.event.ActionEvent evt) {
         String gedung = gedungInput.getSelectedItem().toString();
         String namaRuangan = namaRuanganInput.getSelectedItem().toString();
         String sesi = sesiInput.getSelectedItem().toString();
         String status = statusShow.getText();
-        
+
     }
-    
+
     public static void main(String[] args) {
         PeminjamanRuanganLayout peminjaman = new PeminjamanFunction();
         peminjaman.setVisible(true);
--- src/action/PengelolaanFunction.java
+++ src/action/PengelolaanFunction.java
@@ -3,94 +3,119 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package action;
+
 import java.util.List;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import view.Pengelolaan;
+import java.sql.*;
+
 /**
  *
  * @author USER
  */
-public class PengelolaanFunction extends Pengelolaan{
+public class PengelolaanFunction extends Pengelolaan {
     List listkode = new ArrayList<String>();
     DefaultTableModel model;
     private int baristeerpilih;
-    public PengelolaanFunction(){
-        String[]judul = {"Kode Peminjaman","Nama","Kode Ruangan","Tanggal Pinjam","Sesi"};
-        model = new DefaultTableModel(judul,0);
-        String[]row1 = {"G65","Bintang","G203","13-06-2022","4"};
-        String[]row2 = {"G78","Saman","G206","14-06-2022","4"};
+    Connection conn = null;
+    ResultSet rs = null;
+    PreparedStatement pst = null;
+    Statement st = null;
+    int rows = 0;
+
+    public PengelolaanFunction() {
+        conn = Koneksi.koneksi();
+        String[] judul = { "Kode Peminjaman", "Nama", "Kode Ruangan", "Tanggal Pinjam", "Sesi" };
+        model = new DefaultTableModel(judul, 0);
+        String[] row1 = { "G65", "Bintang", "G203", "13-06-2022", "4" };
+        String[] row2 = { "G78", "Saman", "G206", "14-06-2022", "4" };
         tabelPengelolaan.setModel(model);
+        String sql = "Select tblRuangan.idRuangan,tblRuangan.namaRuangan,tblFasilitas.namaFasilitas,tblRuangan.kapRuangan,tblRuangan.statSesi1,tblRuangan.statSesi2,tblRuangan.statSesi3,tblRuangan.statSesi4 from tblRuangan join tblFasilitas on tblRuangan.idFasilitas=tblFasilitas.idFasilitas";
+
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+            while (rs.next()) {
+
+                model.addRow(new String[] { rs.getString(1), "Bintang", rs.getString(2), "13-06-2022", "2" });
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
         model.addRow(row1);
-        model.addRow(row2);;
+        model.addRow(row2);
+        ;
         jPanel1.setVisible(false);
         jPanel2.setVisible(false);
         System.out.println(tabelPengelolaan.getRowCount());
-        
-        for (int i =0;i< tabelPengelolaan.getRowCount();i++){
-            listkode.add(tabelPengelolaan.getValueAt(i, 0));
-        }
+
+        // for (int i = 0; i < tabelPengelolaan.getRowCount(); i++) {
+        // listkode.add(tabelPengelolaan.getValueAt(i, 0));
+        // }
         cariButton.addActionListener(new java.awt.event.ActionListener() {
-        public void actionPerformed(java.awt.event.ActionEvent evt) {
-        cariButtonActionPerformed(evt);
-    }
-    });
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cariButtonActionPerformed(evt);
+            }
+        });
         okButton.addActionListener(new java.awt.event.ActionListener() {
-        public void actionPerformed(java.awt.event.ActionEvent evt) {
-        okButtonActionPerformed(evt);
-      }
-    });
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                okButtonActionPerformed(evt);
+            }
+        });
         kembaliButton.addActionListener(new java.awt.event.ActionListener() {
-        public void actionPerformed(java.awt.event.ActionEvent evt) {
-        kembaliButtonActionPerformed(evt);
-    }
-    });
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                kembaliButtonActionPerformed(evt);
+            }
+        });
         simpanButton.addActionListener(new java.awt.event.ActionListener() {
-        public void actionPerformed(java.awt.event.ActionEvent evt) {
-        simpanButtonActionPerformed(evt);
-    }
-    });
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                simpanButtonActionPerformed(evt);
+            }
+        });
         cancelButton.addActionListener(new java.awt.event.ActionListener() {
-        public void actionPerformed(java.awt.event.ActionEvent evt) {
-        cancelButtonActionPerformed(evt);
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cancelButtonActionPerformed(evt);
+            }
+        });
+
     }
-    });
-        
-        
-    }
-    private void cariButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
+
+    private void cariButtonActionPerformed(java.awt.event.ActionEvent evt) {
         String kode = kodeInput.getText();
         System.out.println(kode);
-        for (int i =0;i<listkode.size();i++){
-        if (kode.equals(listkode.get(i))){
-            System.out.println("Berhasil mas");
-            baristeerpilih = i;
-            jPanel1.setVisible(true);
-            break;
+        for (int i = 0; i < listkode.size(); i++) {
+            if (kode.equals(listkode.get(i))) {
+                System.out.println("Berhasil mas");
+                baristeerpilih = i;
+                jPanel1.setVisible(true);
+                break;
+            } else {
+
+            }
         }
-        else{
-            
+    }
+
+    private void okButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        boolean hapus = hapusRadioButton.isSelected();
+        boolean edit = editRadioButton.isSelected();
+        if (hapus == true) { // fungsi untuk menghapus peminjaman ruangan dari sisi admin
+            jPanel1.setVisible(false);
+            model.removeRow(baristeerpilih);
+        } else if (edit == true) {
+            jPanel1.setVisible(false);
+            jPanel2.setVisible(true);
         }
-        }
     }
-    private void okButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
-    boolean hapus = hapusRadioButton.isSelected();
-    boolean edit = editRadioButton.isSelected();
-    if (hapus == true){ //fungsi untuk menghapus peminjaman ruangan dari sisi admin
-        jPanel1.setVisible(false);
-    model.removeRow(baristeerpilih);
-    }
-    else if(edit == true){
-    jPanel1.setVisible(false);
-    jPanel2.setVisible(true);
-    }
-    }
-    private void kembaliButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
+
+    private void kembaliButtonActionPerformed(java.awt.event.ActionEvent evt) {
         jPanel1.setVisible(false);
         kodeInput.setText("");
     }
-    private void simpanButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
+
+    private void simpanButtonActionPerformed(java.awt.event.ActionEvent evt) {
         String namaRuangan = namaRuanganInput.getText();
         String tanggalPinjam = tanggalPinjamInput.getText();
         String sesi = sesiInput.getText();
@@ -103,12 +128,13 @@ public class PengelolaanFunction extends Pengelolaan{
         sesiInput.setText("");
         kodeInput.setText("");
     }
-    private void cancelButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
-    
+
+    private void cancelButtonActionPerformed(java.awt.event.ActionEvent evt) {
+
         jPanel2.setVisible(false);
         kodeInput.setText("");
     }
-    
+
     public static void main(String[] args) {
         Pengelolaan kelola = new PengelolaanFunction();
         kelola.setVisible(true);
